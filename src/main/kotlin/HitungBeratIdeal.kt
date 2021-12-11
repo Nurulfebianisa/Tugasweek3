58084d58864e91a0ab3946022cbd4cec832f5926
Nurulfebianisa <92684819+Nurulfebianisa@users.noreply.github.com>
2021-12-11 17:23:23 +0700

    Add files via upload

---
+++ src/main/kotlin/HitungBeratIdeal.kt
@@ -0,0 +1,14 @@
+
+    fun main() {
+        val tinggibadan: Int = 153
+        val beratBadan: Int = 47
+
+        val beratBadanIdeal: Int = tinggibadan - 110
+
+        if (beratBadan <= beratBadanIdeal) {
+            println("kurus")
+        } else if (beratBadan == beratBadanIdeal) {
+            println("ideal")
+        } else if (beratBadan >= beratBadan)
+            println("gemuk")
+    }
